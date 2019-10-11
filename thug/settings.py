966188efbb7f2a1b9694e321ffa544733f8ac952
abhay <abhay.katheria1998@gmail.com>
2019-10-11 14:25:48 +0530

    second

--- thug/settings.py
+++ thug/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,4 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'site_media')
 MEDIA_URL = 'site_media/'
+django_heroku.settings(locals())
